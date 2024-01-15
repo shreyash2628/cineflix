ea1aa098e3955a5b6dfd82261dc35ca66a040d05
Shreyash Pawar <shreyash.pawar26@gmail.com>
2024-01-15 23:36:01 +0530

    Deployed app to Firebase

--- src/Components/UserValidation/Login.js
+++ src/Components/UserValidation/Login.js
@@ -6,7 +6,7 @@ const Login = () => {
     <div className='flex items-center justify-center h-screen '>
     <img className='h-screen w-screen' src={background} alt="Background"></img>
     <div className='absolute top-0 flex flex-col items-center justify-center w-full h-full'>
-      <h1 className='w-96 absolute top-0 left-0   text-red-500 text-5xl' >VDO-GPT</h1>
+      {/* <h1 className='w-96 absolute top-0 left-0   text-red-500 text-5xl' >VDO-GPT</h1> */}
       <UserAuthForm className='opacity-50'/>
     </div>
   </div>
